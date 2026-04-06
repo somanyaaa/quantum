a26106660152a2421e696ead7d7d650a96227263
Samikshaa-007 <samikshaa07s@gmail.com>
2026-04-06 15:49:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -67,4 +67,5 @@ app.get('/api/match/:id', async (req, res) => {
 });
 
 // Start the server
-app.listen(5000, () => console.log("🚀 Server running on Port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on Port ${PORT}`));
